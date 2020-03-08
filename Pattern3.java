b58909b7ee340acf986dd3e34020f7eb45460db4
AshwiniF <ashwinifiske8@gamil.com>
2020-03-08 16:12:36 +0530

    commited

---
+++ Pattern3.java
@@ -0,0 +1,20 @@
+package com.DailyAssignment;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5;i>=0;i--) {
+			for(int s=1;s<=i;s++) {
+				System.out.print(" ");
+			}
+			for(int j=5;j>=i;j--) {
+				System.out.print(" #");
+			}
+			System.out.println();
+		}
+
+	}
+	}
+
+
